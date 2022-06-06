bb657f733a78ff3d95c919cf149b73ae6afbb935
ZalgoNoise <zalgo.noise@gmail.com>
2022-06-06 23:30:27 +0200

    new: adding example

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/zalgonoise/wordgraph/graph"
+)
+
+func main() {
+	keyword := "gopher"
+	target := "logic"
+
+	w, err := graph.FromWordList(os.Getenv("WORD_LIST"))
+	if err != nil {
+		fmt.Printf("failed to grab word list: %v -- please check if your ${WORD_LIST} env variable is set", err)
+		os.Exit(1)
+	}
+
+	n := graph.New()
+	n.Insert(w...)
+
+	result, err := n.FindRoute(keyword, target)
+	if err != nil {
+		fmt.Printf("failed to find route with an error: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(result)
+
+}
